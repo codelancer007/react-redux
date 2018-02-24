9bc5cbdd683865698df566e9c050345d9dc981b3
krishnapulapa <krishna.pulapa@sciits.com>
2018-02-24 12:32:03 +0530

    setup
    setup

---
+++ src/components/common/FieldInput.js
@@ -0,0 +1,38 @@
+import React, {PropTypes} from 'react';
+
+
+const FieldInput = ({input, type, name, label, placeholder, required, meta: {touched, error, warning}}) => {
+    return(
+            <div className="form-group">
+                <label htmlFor={name}>{label}</label>
+
+                <div className="field">
+                    <input 
+                        {...input} 
+                        type={type}
+                        name={name}
+                        className="form-control"
+                        placeholder={placeholder} 
+                    />
+                    
+                    {touched && (required === "1") && ((error && <p className="text-danger">{error}</p>) || (warning && <p className="text-danger">{warning}</p>))}
+                </div>
+            </div>
+    );
+};
+
+
+
+FieldInput.propTypes = {
+    input: PropTypes.object.isRequired,
+    type: PropTypes.string.isRequired,   
+    name: PropTypes.string,
+    label: PropTypes.string.isRequired,
+    placeholder: PropTypes.string,
+	required: PropTypes.string,
+    meta: PropTypes.object.isRequired,
+};
+
+
+
+export default FieldInput;
